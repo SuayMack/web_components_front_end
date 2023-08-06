66a3a2d9da10dae2aeedf8bd7b23d8452d8dbed7
SuayMack <pry_chu@hotmail.com>
2023-08-06 15:49:02 -0300

    UPDATE - build - appendchild cardLeft/cardRight

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -14,8 +14,19 @@ class CardNews extends HTMLElement {
     const cardLeft = document.createElement("div");
     cardLeft.setAttribute("class", "card-left");
     
+    const autor = document.createElement("span");
+    const linkTitle = document.createElement("a");
+    const newsContent = document.createElement("p");
+
+    cardLeft.appendChild(autor);
+    cardLeft.appendChild(linkTitle);
+    cardLeft.appendChild(newsContent);
+    
     const cardRight = document.createElement("div");
     cardRight.setAttribute("class", "card-right");
+
+    const newsImage = document.createElement("img");
+    cardRight.appendChild(newsImage);
 
     componentRoot.appendChild(cardLeft);
     componentRoot.appendChild(cardRight)
